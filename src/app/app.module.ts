3114e60ad7eec159f05fb7f5b987c841ac975b19
CeAlCuadrado <cealcuadrado@outlook.com>
2021-05-09 14:03:06 -0400

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
